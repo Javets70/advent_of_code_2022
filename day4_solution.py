82449afc4aa1c67e179a4ce3383f7139bfffc62b
JavetsM <javetsm70@gmail.com>
2023-02-17 14:12:04 +0530

    DAY 5 Part 1

--- day4_solution.py
+++ day4_solution.py
@@ -25,7 +25,24 @@ class pairComparison:
                 self.count += 1
         return self.count
 
+    @staticmethod
+    def checkIntersection(range1: list, range2: list):
+        set1 = set(range(range1[0], range1[-1] + 1))
+        set2 = set(range(range2[0], range2[-1] + 1))
+        if set1.intersection(set2):
+            return True
+
+    def getAns2(self):
+        self.count = 0
+        for line in self.rdata:
+            range1, range2 = self.getPair(line)
+            if self.checkIntersection(range1, range2):
+                self.count += 1
+
+        return self.count
+
 
 # test = pairComparison("./day4_sample_input.txt")
 test = pairComparison("day4_input.txt")
-print(test.getAns1())
+# print(test.getAns1())
+print(test.getAns2())
---
+++ day5_solution.py
@@ -0,0 +1,52 @@
+class moveCrates:
+    def __init__(self, filePath: str):
+        self.rdata = open(filePath).readlines()
+
+    def getCrates(self):
+        for idx, elem in enumerate(self.rdata):
+            if elem == "\n":
+                self.crates = self.rdata[:idx]
+                self.commands = self.rdata[idx + 1:]
+                break
+
+    def makeCrates(self):
+        self.getCrates()
+        numberOfStacks = int(
+            self.crates[-1].rstrip("\n").split()[-1]
+        )  # remove newline , split it , get last char
+        self.crateDict = {}
+        for i in range(1, numberOfStacks + 1):
+            self.crateDict[i] = []  # create empty crateDict
+
+        # Add elements to crateDict
+        for _, elem in enumerate(self.crates[:-1]):
+            for idx2, elem2 in enumerate(elem):
+                if idx2 % 4 == 1 and elem2.isupper():  # check if elem is not space
+                    self.crateDict[idx2 // 4 + 1].append(elem2)
+
+        for command in self.commands:
+            command = command.split()
+            count, _from, to = int(command[1]), int(
+                command[3]), int(command[5])
+            self.moveCrates(self.crateDict, count, _from, to)
+
+        return self.crateDict
+
+    @staticmethod
+    def moveCrates(crateDict: dict, count: int, _from: int, to: int):
+        cratesToMove = crateDict[_from][:count]
+        crateDict[_from] = crateDict[_from][count:]
+        for i in cratesToMove:
+            crateDict[to].insert(0, i)
+
+    def getAns1(self):
+        self.makeCrates()
+        output_str = ""
+        for item in self.crateDict.values():
+            output_str += item[0]
+        return output_str
+
+
+# test = moveCrates("./day5_sample_input.txt")
+test = moveCrates("./day5_input.txt")
+print(test.getAns1())
